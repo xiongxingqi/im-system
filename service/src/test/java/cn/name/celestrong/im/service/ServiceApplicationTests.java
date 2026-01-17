60a0f0d1b5c64fb6b8bf50986cf8d2e26b651a88
celestrong <celestrongcn@gmail.com>
2026-01-17 15:23:48 +0800

    init: 项目初始化，开启0.0.x-SNAPSHOT版本的开发

---
+++ service/src/test/java/cn/name/celestrong/im/service/ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.name.celestrong.im.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
