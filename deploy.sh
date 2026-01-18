612cc1e90a2a91c5d8d8f5c6e49c4bf5761f12b2
celestrong <celestrongcn@gmail.com>
2026-01-18 15:52:37 +0800

    feat(log-tool.sh): 优化deploy.sh脚本

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 # 设置为严格模式
 set -euo pipefail
+readonly LOG_PREFIX="im-system"
 source ./log-tool.sh
-
-readonly  APP_NAME="im-service"
+readonly APP_NAME="im-service"
 
 main(){
   local version
+  local deploy_env="${1}"
+
+  if [[ "${deploy_env:-}" != "test" && "${deploy_env:-}" != "pro" ]]; then
+      err "请输入正确的部署环境！test OR pro"
+  fi
   # 获取项目版本号
   version=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 
