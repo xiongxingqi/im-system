b15b34a27af3c85d9cc3d16a5cbdad799f93eb62
celestrong <celestrongcn@gmail.com>
2026-01-18 17:56:24 +0800

    feat(deploy.sh): 添加service镜像的构建

--- deploy.sh
+++ deploy.sh
@@ -6,21 +6,22 @@ source ./log-tool.sh
 readonly APP_NAME="im-service"
 
 main(){
-  local version
   local deploy_env="${1}"
 
   if [[ "${deploy_env:-}" != "test" && "${deploy_env:-}" != "pro" ]]; then
       err "请输入正确的部署环境！test OR pro"
   fi
   # 获取项目版本号
-  version=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+  local app_version
+  app_version=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 
-  info "开始构建项目,当前版本：${version}"
+  info "开始构建项目,当前版本：${app_version}"
   mvn clean package -DskipTests
 
   info "开始构建项目docker镜像！"
 
-  docker build -t "${APP_NAME}:${version}" .
+  docker build --build-arg APP_VERSION="${app_version}" -t "${APP_NAME}:${app_version}" .
+
 
 }
 
